95916b0fafc758a67db16b4fcc7814eec14e4a7d
GustavHenry <gustav_h@icloud.com>
2022-11-01 13:23:42 +0800

    finishing main branch

--- src/keys.py
+++ src/keys.py
@@ -3,9 +3,17 @@ MODELS_DIR = "../models"
 REPORTS_DIR = '../reports'
 NOTEBOOKS_DIR = "../notebooks"
 
+
 DATA_RAW_DIR = "../data/raw"
 DATA_INT_DIR = "../data/interim"
 DATA_PRC_DIC = "../data/processed"
 
 DATA_RAW_INFRARED_DIR = "../data/raw/InfraredImages"
 DATA_RAW_DIR_ELECTROLYZER = "../data/raw/Electrolyzer"
+
+class DataDir:
+    Raw = "../data/raw"
+    Int = "../data/interim"
+    Prc = "../data/processed"
+    Raw_infrared = "../data/raw/InfraredImages"
+    Raw_electrolyzer = "../data/raw/Electrolyzer"
