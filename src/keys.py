fbb964d4a65fbb39bcf9d146fccfc3b69556f435
GustavHenry <gustav_h@icloud.com>
2022-11-01 09:57:16 +0800

    adding infrared images

---
+++ src/keys.py
@@ -0,0 +1,11 @@
+CACHE_DIR = "../.cache"
+MODELS_DIR = "../models"
+REPORTS_DIR = '../reports'
+NOTEBOOKS_DIR = "../notebooks"
+
+DATA_RAW_DIR = "../data/raw"
+DATA_INT_DIR = "../data/interim"
+DATA_PRC_DIC = "../data/processed"
+
+DATA_RAW_INFRARED_DIR = "../data/raw/InfraredImages"
+DATA_RAW_DIR_ELECTROLYZER = "../data/raw/Electrolyzer"
